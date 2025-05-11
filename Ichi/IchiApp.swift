00b5f429c9bf8a36b704b7b14bb7d0be1634135a
Rudrank Riyam <rudrankriyam@gmail.com>
2025-05-12 00:27:24 +0530

    Initial project setup

---
+++ Ichi/IchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IchiApp.swift
+//  Ichi
+//
+//  Created by Rudrank Riyam on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
